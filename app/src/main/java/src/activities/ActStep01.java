54cc1bde98971ee141b20b6a08c53d92b6db34b9
waps12b <waps12b@naver.com>
2015-03-15 14:33:52 +0900

    step 01 activity

---
+++ app/src/main/java/src/activities/ActStep01.java
@@ -0,0 +1,21 @@
+package src.activities;
+
+import android.os.Bundle;
+
+import cdmst.smartsilver.R;
+
+/**
+ * Created by waps12b on 15. 3. 15..
+ */
+public class ActStep01 extends FrameActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+}
