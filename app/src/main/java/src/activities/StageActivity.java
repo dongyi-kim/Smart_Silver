717ffab4e5066d01e54235c85c16773c300f5d9b
waps12b <waps12b@naver.com>
2015-03-26 18:04:22 +0900

    final method

--- app/src/main/java/src/activities/StageActivity.java
+++ app/src/main/java/src/activities/StageActivity.java
@@ -29,6 +29,7 @@ public abstract class StageActivity extends FrameActivity {
     {
         arrResult[iStage].Stop(bResult);
     }
+
     /* void setQustion(boolean isRetry, Object object)
      * - object는 옵션사항. 필요할 시 사용
      * - 각 Stage의 문제에 대한 초기화를 진행
@@ -38,7 +39,7 @@ public abstract class StageActivity extends FrameActivity {
      * - 새로운 보기 설정 시 Listener를 통해 checkAnswer()를 호출
      */
     public abstract void setQuestion( boolean isRetry, Object object);
-    public void setQuestion(boolean isRetry) { setQuestion(isRetry, null); }
+    public final void setQuestion(boolean isRetry) { setQuestion(isRetry, null); }
 
 
     /* void checkAnswer(Object object)
@@ -49,14 +50,14 @@ public abstract class StageActivity extends FrameActivity {
      * -
      */
     public abstract void checkAnswer(Object object);
-    public void checkAnswer() { checkAnswer(null); }
+    public final void checkAnswer() { checkAnswer(null); }
 
     /* void goNext()
      * - iStage > NUM_OF_STAGE 가 될 시 checkAnswer()에 의해 호출 됨.
      * - 다음 Level혹은 Step을 호출
      */
     public abstract void goNext(Object object);
-    public void goNext() { goNext(null); };
+    public final void goNext() { goNext(null); };
 
 
 }
