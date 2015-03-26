4c40ebbb8e056f4836051743e52aadd92464cf8a
waps12b <waps12b@naver.com>
2015-03-26 15:27:18 +0900

    function modified

--- app/src/main/java/src/activities/StageActivity.java
+++ app/src/main/java/src/activities/StageActivity.java
@@ -31,26 +31,35 @@ public abstract class StageActivity extends FrameActivity {
     }
 
 
-    /* void setQustion()
+    /* void setQustion(boolean isRetry, Object object)
+     * - object는 옵션사항. 필요할 시 사용
      * - 각 Stage의 문제에 대한 초기화를 진행
      * - 문제와 보기를 초기화
      * - isRetry가 false면 답 입력창을 초기화한 후 return,
      * - isRetry가 true면 문제와 보기를 새롭게 설정
      * - 새로운 보기 설정 시 Listener를 통해 checkAnswer()를 호출
      */
-    public abstract void setQuestion(boolean isRetry);
 
-    /* void checkAnswer()
+    public abstract void setQuestion( boolean isRetry, Object object);
+    public void setQuestion(boolean isRetry) { setQuestion(isRetry, null); }
+
+
+    /* void checkAnswer(Object object)
+     * - object는 선택사항 필요할 시 사용.
      * - iStage를 1 증가시켜줌, 이 때 iStage > NUM_OF_STAGE면 goNext();
      * - DlgResultMark로 결과 통보
      * - 정답이면 setQuestion()을 호출하여 새로운 문제 준비
+     * -
      */
-    public abstract void checkAnswer();
+    public abstract void checkAnswer(Object object);
+    public void checkAnswer() { checkAnswer(null); }
 
     /* void goNext()
      * - iStage > NUM_OF_STAGE 가 될 시 checkAnswer()에 의해 호출 됨.
      * - 다음 Level혹은 Step을 호출
      */
-    public abstract void goNext();
+    public abstract void goNext(Object object);
+    public void goNext() { goNext(null); };
+
 
 }
