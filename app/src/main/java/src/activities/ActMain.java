f280221a749dae9d67d2f8092a5da5cae9cc7053
unknown <Acka1357@gmail.com>
2015-04-08 01:57:06 +0900

    0304 added

--- app/src/main/java/src/activities/ActMain.java
+++ app/src/main/java/src/activities/ActMain.java
@@ -45,7 +45,7 @@ public class ActMain extends FrameActivity{
 
             }else if(v == btnStep02)
             {
-                Intent intent = new Intent(v.getContext(), ActStep0303.class);
+                Intent intent = new Intent(v.getContext(), ActStep0304.class);
                 startActivity(intent);
 
             }else if(v== btnStep03)
