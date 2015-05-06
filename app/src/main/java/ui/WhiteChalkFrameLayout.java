e22574337c3b6b6bca4499cd003ac038b0e2d98e
waps12b <waps12b@naver.com>
2015-05-06 11:03:32 +0900

    dp changed

--- app/src/main/java/ui/WhiteChalkFrameLayout.java
+++ app/src/main/java/ui/WhiteChalkFrameLayout.java
@@ -9,18 +9,18 @@ import src.viewes.FrameView;
 /**
  * Created by waps12b on 15. 5. 6..
  */
-public class ChalkFrameLayout extends FrameView {
-    public ChalkFrameLayout(Context context) {
+public class WhiteChalkFrameLayout extends FrameView {
+    public WhiteChalkFrameLayout(Context context) {
         this(context, null);
     }
 
-    public ChalkFrameLayout(Context context, AttributeSet attrs) {
+    public WhiteChalkFrameLayout(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public ChalkFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+    public WhiteChalkFrameLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setBackgroundImage(R.drawable.img_chalkboard_2);
+        setBackgroundImage(R.drawable.img_chalkboard_2_white);
         //setLayout(R.layout.view_chalk_board);
         setPadding(40,40,40,40);
 
