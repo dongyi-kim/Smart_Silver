0572a8efbcfbfe58edc22d9b35af43b5512f9b15
waps12b <waps12b@naver.com>
2015-05-20 16:49:52 +0900

    statistics timer added

--- app/src/main/java/src/activities/ResultPage/StatisticsActivity.java
+++ app/src/main/java/src/activities/ResultPage/StatisticsActivity.java
@@ -1,8 +1,11 @@
 package src.activities.ResultPage;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import cdmst.smartsilver.R;
+import src.DB;
+import src.ResultData;
 import src.activities.FrameActivity;
 
 /**
@@ -10,9 +13,32 @@ import src.activities.FrameActivity;
  */
 public class StatisticsActivity extends FrameActivity {
 
+    private int get_minute(String query)
+    {
+        long millisec = 0;
+        ResultData[] data = DB.SELECT(query);
+        if(data == null)
+            return 0;
+
+        for(int i=0; i<data.length; i++)
+        {
+            millisec += data[i].millisec;
+        }
+        return (int)(millisec / 60000);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_statistics_main);
+
+        //set play time
+        TextView txtToday =  (TextView)findViewById(R.id.txt_playtime_today);
+        txtToday.setText( get_minute("SELECT * FROM RESULT_DATA WHERE timestamp >= date('now');") + "분");
+
+        TextView txtTotal =  (TextView)findViewById(R.id.txt_playtime_total);
+        txtTotal.setText( get_minute("SELECT * FROM RESULT_DATA;") + "분");
+
     }
+
+
 }
