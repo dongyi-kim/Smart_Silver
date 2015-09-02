cac419e20f7243fe33757efdaa125c815229b5d9
Acka03 <Acka1357@gmail.com>
2015-09-02 19:14:09 +0900

    0401~0402

--- app/src/main/java/src/activities/Step04/ActStep0401.java
+++ app/src/main/java/src/activities/Step04/ActStep0401.java
@@ -22,7 +22,7 @@ import src.dialogs.DlgResultMark;
  */
 public class ActStep0401 extends StageActivity{
     private final ImageView imgPicture[] = new ImageView[2];
-    public final ImageButton ibtnAnswer[] = new ImageButton[2];
+    public final Button btnAnswer[] = new Button[2];
     public final TextView txtAnswer[] = new TextView[2];
 
     private int iRetryCount = 0;
@@ -38,18 +38,17 @@ public class ActStep0401 extends StageActivity{
 
         imgPicture[0] = (ImageView)findViewById(R.id.img_picture_1);
         imgPicture[1] = (ImageView)findViewById(R.id.img_picture_2);
-        ibtnAnswer[0] = (ImageButton)findViewById(R.id.ibtn_answer_1);
-        ibtnAnswer[1] = (ImageButton)findViewById(R.id.ibtn_answer_2);
+        btnAnswer[0] = (Button)findViewById(R.id.btn_answer_1);
+        btnAnswer[1] = (Button)findViewById(R.id.btn_answer_2);
         txtAnswer[0] = (TextView)findViewById(R.id.txt_answer_1);
         txtAnswer[1] = (TextView)findViewById(R.id.txt_answer_2);
 
         for(int i = 0; i < 2; i++)
-            ibtnAnswer[i].setOnClickListener(new View.OnClickListener(){
+            btnAnswer[i].setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){
-                    int iSelectIndex = (v == ibtnAnswer[0]? 0 : 1);
-                    if(dataSet.iAnswerCount[iSelectIndex][0] == dataSet.iPictureCount[iSelectIndex][0] &&
-                            dataSet.iAnswerCount[iSelectIndex][1] == dataSet.iPictureCount[iSelectIndex][1]) isRight = true;
+                    int iSelectIndex = (v == btnAnswer[0]? 0 : 1);
+                    if(dataSet.bIsAnswer[iSelectIndex]) isRight = true;
                     else isRight = false;
                     checkAnswer();
                 }
@@ -59,19 +58,11 @@ public class ActStep0401 extends StageActivity{
     }
 
     public void setQuestion(boolean isRetry, Object object){
-        int iRandomSeed = iStage - 1;
-        dataSet.setData(iRandomSeed);
+        dataSet.setData(iStage);
 
         for(int i = 0; i < 2; i++){
-            imgPicture[i].setImageResource(dataSet.iPictureSource[i]);
-
-            String sAnswer = "";
-            sAnswer += dataSet.iAnswerCount[i][0];
-            if(dataSet.isFlower[i]) sAnswer += "송이씩\n";
-            else sAnswer += "개씩\n";
-            sAnswer += dataSet.iAnswerCount[i][1] + "묶음";
-
-            txtAnswer[i].setText(sAnswer);
+            imgPicture[i].setImageResource(dataSet.iImageResource[i]);
+            txtAnswer[i].setText(dataSet.sCountDescription[i]);
         }
 
         StartRecording();
@@ -105,47 +96,64 @@ public class ActStep0401 extends StageActivity{
 
     public class Step0401DataSet{
         //private enum ePictureType {goodgam, hongshi, apple, flower, waterhippo, cherry};
-        private final int goodgam = 0, hongshi = 1, apple = 2, flower = 3, waterhippo = 4, cherry = 5;
+        private final int yakgwa = 0, dried_persimmon = 1, pumpkin_pancake = 2, songpyeon = 3, apple = 4, flower = 5;
+        private final int MAX_SET_NUMBER = 4;
 
-        private final int arrImageSource[][][] = new int[26][11][11];
-        private final int arrPictureCount[][][] = {{{2, 2}, {5, 2}},
-                {{4, 3}, {5, 2}},
-                {{4, 1}, {5, 2}},
-                {{3, 6}, {2, 9}},
-                {{7, 2}, {5, 3}}};
-        private final int arrAnswerCount[][][] = {{{5, 2}, {5, 2}},
-                {{4, 3}, {5, 3}},
-                {{4, 2}, {5, 2}},
-                {{3, 6}, {2, 6}},
-                {{7, 3}, {5, 3}}};
-        private final int arrImageType[][] = {{goodgam, hongshi}, {apple, hongshi}, {apple, flower}, {apple, cherry}, {waterhippo, flower}};
+        private final String arrCountUnit[] = {"개", "개", "개", "개", "개", "송이"};
+        private final int arrRandomBase[] = {2, 3, 2, 2, 2, 2};
+        private final int arrRandomRange[] = {5, 4, 5, 5, 5, 3};
+        private final int arrImageSource[][][] = {{{}, {}, {0, R.drawable.img_mult_yakgwa_2_1, R.drawable.img_mult_yakgwa_2_2, R.drawable.img_mult_yakgwa_2_3, R.drawable.img_mult_yakgwa_2_4},
+                {0, R.drawable.img_mult_yakgwa_3_1, R.drawable.img_mult_yakgwa_3_2, R.drawable.img_mult_yakgwa_3_3, R.drawable.img_mult_yakgwa_3_4},
+                {0, R.drawable.img_mult_yakgwa_4_1, R.drawable.img_mult_yakgwa_4_2, R.drawable.img_mult_yakgwa_4_3, R.drawable.img_mult_yakgwa_4_4},
+                {0, R.drawable.img_mult_yakgwa_5_1, R.drawable.img_mult_yakgwa_5_2, R.drawable.img_mult_yakgwa_5_3, R.drawable.img_mult_yakgwa_5_4},
+                {0, R.drawable.img_mult_yakgwa_6_1, R.drawable.img_mult_yakgwa_6_2, R.drawable.img_mult_yakgwa_6_3, R.drawable.img_mult_yakgwa_6_4}},
+                {{}, {}, {}, {0, R.drawable.img_mult_dried_persimmon_3_1, R.drawable.img_mult_dried_persimmon_3_2, R.drawable.img_mult_dried_persimmon_3_3, R.drawable.img_mult_dried_persimmon_3_4},
+                        {0, R.drawable.img_mult_dried_persimmon_4_1, R.drawable.img_mult_dried_persimmon_4_2, R.drawable.img_mult_dried_persimmon_4_3, R.drawable.img_mult_dried_persimmon_4_4},
+                        {0, R.drawable.img_mult_dried_persimmon_5_1, R.drawable.img_mult_dried_persimmon_5_2, R.drawable.img_mult_dried_persimmon_5_3, R.drawable.img_mult_dried_persimmon_5_4},
+                        {0, R.drawable.img_mult_dried_persimmon_6_1, R.drawable.img_mult_dried_persimmon_6_2, R.drawable.img_mult_dried_persimmon_6_3, R.drawable.img_mult_dried_persimmon_6_4}},
+                {{}, {}, {0, R.drawable.img_mult_pumpkin_pancake_2_1, R.drawable.img_mult_pumpkin_pancake_2_2, R.drawable.img_mult_pumpkin_pancake_2_3, R.drawable.img_mult_pumpkin_pancake_2_4},
+                        {0, R.drawable.img_mult_pumpkin_pancake_3_1, R.drawable.img_mult_pumpkin_pancake_3_2, R.drawable.img_mult_pumpkin_pancake_3_3, R.drawable.img_mult_pumpkin_pancake_3_4},
+                        {0, R.drawable.img_mult_pumpkin_pancake_4_1, R.drawable.img_mult_pumpkin_pancake_4_2, R.drawable.img_mult_pumpkin_pancake_4_3, R.drawable.img_mult_pumpkin_pancake_4_4},
+                        {0, R.drawable.img_mult_pumpkin_pancake_5_1, R.drawable.img_mult_pumpkin_pancake_5_2, R.drawable.img_mult_pumpkin_pancake_5_3, R.drawable.img_mult_pumpkin_pancake_5_4},
+                        {0, R.drawable.img_mult_pumpkin_pancake_6_1, R.drawable.img_mult_pumpkin_pancake_6_2, R.drawable.img_mult_pumpkin_pancake_6_3, R.drawable.img_mult_pumpkin_pancake_6_4}},
+                {{}, {}, {0, R.drawable.img_mult_songpyeon_2_1, R.drawable.img_mult_songpyeon_2_2, R.drawable.img_mult_songpyeon_2_3, R.drawable.img_mult_songpyeon_2_4},
+                        {0, R.drawable.img_mult_songpyeon_3_1, R.drawable.img_mult_songpyeon_3_2, R.drawable.img_mult_songpyeon_3_3, R.drawable.img_mult_songpyeon_3_4},
+                        {0, R.drawable.img_mult_songpyeon_4_1, R.drawable.img_mult_songpyeon_4_2, R.drawable.img_mult_songpyeon_4_3, R.drawable.img_mult_songpyeon_4_4},
+                        {0, R.drawable.img_mult_songpyeon_5_1, R.drawable.img_mult_songpyeon_5_2, R.drawable.img_mult_songpyeon_5_3, R.drawable.img_mult_songpyeon_5_4},
+                        {0, R.drawable.img_mult_songpyeon_6_1, R.drawable.img_mult_songpyeon_6_2, R.drawable.img_mult_songpyeon_6_3, R.drawable.img_mult_songpyeon_6_4}},
+                {{}, {}, {0, R.drawable.img_mult_apple_2_1, R.drawable.img_mult_apple_2_2, R.drawable.img_mult_apple_2_3, R.drawable.img_mult_apple_2_4},
+                        {0, R.drawable.img_mult_apple_3_1, R.drawable.img_mult_apple_3_2, R.drawable.img_mult_apple_3_3, R.drawable.img_mult_apple_3_4},
+                        {0, R.drawable.img_mult_apple_4_1, R.drawable.img_mult_apple_4_2, R.drawable.img_mult_apple_4_3, R.drawable.img_mult_apple_4_4},
+                        {0, R.drawable.img_mult_apple_5_1, R.drawable.img_mult_apple_5_2, R.drawable.img_mult_apple_5_3, R.drawable.img_mult_apple_5_4},
+                        {0, R.drawable.img_mult_apple_6_1, R.drawable.img_mult_apple_6_2, R.drawable.img_mult_apple_6_3, R.drawable.img_mult_apple_6_4}},
+                {{}, {}, {0, R.drawable.img_mult_flower_2_1, R.drawable.img_mult_flower_2_2, R.drawable.img_mult_flower_2_3, R.drawable.img_mult_flower_2_4},
+                        {0, R.drawable.img_mult_flower_3_1, R.drawable.img_mult_flower_3_2, R.drawable.img_mult_flower_3_3, R.drawable.img_mult_flower_3_4},
+                        {0, R.drawable.img_mult_flower_4_1, R.drawable.img_mult_flower_4_2, R.drawable.img_mult_flower_4_3, R.drawable.img_mult_flower_4_4}}};
 
-        public final int iPictureCount[][] = new int[2][2];
-        public final int iAnswerCount[][] = new int[2][2];
-        public final int iPictureSource[] = new int[2];
-        public final boolean isFlower[] = new boolean[2];
+        public String sCountDescription[] = new String[2];
+        public int iImageResource[] = new int[2];
+        public boolean bIsAnswer[] = new boolean[2];
 
-        public Step0401DataSet(){
-            arrImageSource[goodgam][2][2] = R.drawable.mult_2_2_goodgam;
-            arrImageSource[hongshi][5][2] = R.drawable.mult_5_2_hongshi;
-            arrImageSource[apple][4][1] = R.drawable.mult_4_1_apple;
-            arrImageSource[apple][4][3] = R.drawable.mult_4_3_apple;
-            arrImageSource[apple][3][6] = R.drawable.mult_3_6_apple;
-            arrImageSource[flower][5][2] = R.drawable.mult_5_2_flower;
-            arrImageSource[flower][5][3] = R.drawable.mult_5_3_flower;
-            arrImageSource[waterhippo][7][2] = R.drawable.mult_7_2_waterhippo;
-            arrImageSource[cherry][2][9] = R.drawable.mult_2_9_cherry;
-        }
+        public void setData(int iStage) {
+            int iSeed = rand.nextInt(3);
 
-        public void setData(int iSeed){
-            for(int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    iPictureCount[i][j] = arrPictureCount[iSeed][i][j];
-                    iAnswerCount[i][j] = arrAnswerCount[iSeed][i][j];
+            bIsAnswer[0] = rand.nextBoolean(); bIsAnswer[1] = !bIsAnswer[0];
+            for(int i = 0; i < 2; i++){
+                int iSingleSet = arrRandomBase[2 * iSeed + i] + rand.nextInt(arrRandomRange[2 * iSeed + i]);
+                int iSetCount = 1 + rand.nextInt(MAX_SET_NUMBER);
+
+                iImageResource[i] = arrImageSource[2 * iSeed + i][iSingleSet][iSetCount];
+
+                if(!bIsAnswer[i]){
+                    int iRandNum = rand.nextInt(4);
+                    if(iRandNum == 0 && iSingleSet != 2) iSingleSet--;
+                    else if(iRandNum == 1) iSingleSet++;
+                    else if(iRandNum == 2 && iSetCount != 1) iSetCount--;
+                    else if(iRandNum == 3) iSetCount++;
+                    else iSetCount++;
                 }
 
-                iPictureSource[i] = arrImageSource[arrImageType[iSeed][i]][iPictureCount[i][0]][iPictureCount[i][1]];
-                if(arrImageType[iSeed][i] == flower) isFlower[i] = true;
+                sCountDescription[i] = "" + iSingleSet + arrCountUnit[2 * iSeed + i] + "씩\n" + iSetCount + "묶음";
             }
         }
     }
