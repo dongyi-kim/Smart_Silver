1d9d2ae2153bcb9a5cb04153f209d778edf377c7
Acka03 <Acka1357@gmail.com>
2015-09-02 22:17:48 +0900

    step 05 complete

--- app/build/generated/source/r/debug/com/andexert/library/R.java
+++ app/build/generated/source/r/debug/com/andexert/library/R.java
@@ -24,7 +24,7 @@ public final class R {
 	}
 	public static final class drawable {
 		public static final int ic_launcher = 0x7f0200b7;
-		public static final int shape_rounded = 0x7f02023a;
+		public static final int shape_rounded = 0x7f02023d;
 	}
 	public static final class id {
 		public static final int doubleRipple = 0x7f090036;
