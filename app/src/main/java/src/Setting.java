c942c528be6b6821e7d4b9d9f2409a3c59a49cc5
adman <jhoboss@gmail.com>
2015-10-30 10:29:50 +0900

    Merge branch 'Acka' of https://github.com/waps12b/Smart_Silver into adman
    Conflicts:
    	app/build/generated/source/r/debug/cdmst/smartsilver/R.java
    	app/build/generated/source/r/debug/com/andexert/library/R.java
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$drawable.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$id.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$integer.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$layout.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$menu.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$string.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$style.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$styleable.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R$xml.class
    	app/build/intermediates/classes/debug/cdmst/smartsilver/R.class
    	app/build/intermediates/classes/debug/com/andexert/library/R$drawable.class
    	app/build/intermediates/classes/debug/src/activities/ActTest$1.class
    	app/build/intermediates/classes/debug/src/activities/ActTest.class
    	app/build/intermediates/classes/debug/src/activities/Step01/ActStep0102$Step0102DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step01/ActStep0102.class
    	app/build/intermediates/classes/debug/src/activities/Step01/ActStep0105$Step0105NumberSet.class
    	app/build/intermediates/classes/debug/src/activities/Step02/ActStep0201.class
    	app/build/intermediates/classes/debug/src/activities/Step03/ActStep0303$Step0303DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step03/ActStep0304$Step0304DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step03/ActStep0305.class
    	app/build/intermediates/classes/debug/src/activities/Step04/ActStep0401$Step0401DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step04/ActStep0402$Step0402DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step04/ActStep0403$Step0403DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step04/ActStep0404$Step0404DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step04/ActStep0405$Step0405DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step05/ActStep0502$Step0502DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step05/ActStep0503$Step0503DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step05/ActStep0504$Step0504DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step05/ActStep0504.class
    	app/build/intermediates/classes/debug/src/activities/Step05/ActStep0505$Step0505DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step06/Step0601DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step06/Step0602DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step06/Step0604DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step06/Step0605DataSet.class
    	app/build/intermediates/classes/debug/src/activities/Step07/ActStep0701.class
    	app/build/intermediates/classes/debug/src/dialogs/DlgResultMark.class
    	app/build/intermediates/classes/debug/ui/CButton.class
    	app/build/intermediates/classes/debug/ui/ChalkFrameLayout.class
    	app/build/intermediates/classes/debug/ui/MemoFrameLayout.class
    	app/build/intermediates/classes/debug/ui/WhiteChalkFrameLayout.class
    	app/build/intermediates/dex/debug/classes.dex
    	app/build/intermediates/incremental/mergeResources/debug/merger.xml
    	app/build/intermediates/res/resources-debug.ap_
    	app/build/intermediates/symbols/debug/R.txt
    	app/build/outputs/apk/app-debug-unaligned.apk
    	app/build/outputs/apk/app-debug.apk
    	app/src/main/java/src/Setting.java

--- app/src/main/java/src/Setting.java
+++ app/src/main/java/src/Setting.java
@@ -7,7 +7,7 @@ import src.activities.Step04.*;
 import src.activities.Step05.*;
 import src.activities.Step06.*;
 import src.activities.Step07.*;
-
+import src.activities.Step08.*;
 import src.activities.Step09.*;
 import src.activities.Step10.ActStep10;
 
@@ -27,7 +27,7 @@ public class Setting {
             {5,5,5,5,5},
             {5,5,5,5,5},
             {5,5,5,5,5},
-            {4,4,5,4,4},
+            {4,5,5,5,5},
             {10},
     };
 
@@ -39,8 +39,8 @@ public class Setting {
             {ActStep0501.class, ActStep0502.class, ActStep0503.class, ActStep0504.class, ActStep0505.class },//step5
             {ActStep0601.class, ActStep0602.class, ActStep0603.class, ActStep0604.class, ActStep0605.class },//step6
             {ActStep0701.class, ActStep0702.class, ActStep0703.class, ActStep0704.class, ActStep0705.class },//step7
-            { },//step8
-            {ActStep0902.class, ActStep0902.class, ActStep0903.class, ActStep0904.class, ActStep0905.class,  },//step9
+            {ActStep0801.class, ActStep0802.class, ActStep0803.class, ActStep0804.class, ActStep0805.class },//step8
+            {ActStep0902.class, ActStep0903.class, ActStep0904.class, ActStep0905.class,  },//step9
             {ActStep10.class },//step10
     };
 
