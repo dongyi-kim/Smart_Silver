5a2ec3905f7d8d520438959f22d658e09790368d
adman <jhoboss@gmail.com>
2015-12-05 15:19:19 +0900

    step0101, step0201 modify

--- app/src/main/java/src/activities/Step07/ActStep0704.java
+++ app/src/main/java/src/activities/Step07/ActStep0704.java
@@ -2,16 +2,9 @@ package src.activities.Step07;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import cdmst.smartsilver.R;
@@ -19,179 +12,103 @@ import src.activities.ActMain;
 import src.activities.StageActivity;
 import src.dialogs.DlgResultMark;
 
-/**
- * Created by Acka on 2015-08-04.
- */
-public class ActStep0704  extends StageActivity {
-    private static final int BUTTON_COUNT = 5;
-    private static final int CELL_ROW = 4;
-    private static final int CELL_COLUMN = 5;
+public class ActStep0704 extends StageActivity {
 
-    private FrameLayout frame7_4_[] = new FrameLayout[2];
-
-    private LinearLayout linearRequestList;
-    private LinearLayout linearRequestMoney[] = new LinearLayout[2];
-    private LinearLayout linearClientList[] = new LinearLayout[2];
-    private LinearLayout linearDefaultList[] = new LinearLayout[2];
-    private LinearLayout linearUsing[] = new LinearLayout[2];
-    private LinearLayout linearTaxBill[] = new LinearLayout[2];
     private TextView txtDescription;
-    public final Button btnAnswerButton[] = new Button[BUTTON_COUNT];
-
-    private ImageView imgReceipt;
-    public final Button btnSingleCell[][] = new Button[CELL_ROW][CELL_COLUMN];
-    public final int iRowCount[] = {1, 5, 5, 3};
+    private Button btnlist[] = new Button[18];
+    private TextView txtlist[] = new TextView[18];
+    private String money[] = new String[6];
+    private TextView iaccount;
 
     private int iRetryCount = 0;
     public boolean isRight = false;
 
-    public Step0704DataSet dataSet = new Step0704DataSet();
+    public Step0703DataSet dataSet = new Step0703DataSet();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_step_07_4);
+        setContentView(R.layout.act_step_07_3);
 
         txtDescription = (TextView)findViewById(R.id.txt_description);
 
-        frame7_4_[0] = (FrameLayout)findViewById(R.id.frame_7_4_1);
-        frame7_4_[1] = (FrameLayout)findViewById(R.id.frame_7_4_2);
+        btnlist[0] = (Button)findViewById(R.id.btn_1);
+        btnlist[1] = (Button)findViewById(R.id.btn_2);
+        btnlist[2] = (Button)findViewById(R.id.btn_3);
+        btnlist[3] = (Button)findViewById(R.id.btn_4);
+        btnlist[4] = (Button)findViewById(R.id.btn_5);
+        btnlist[5] = (Button)findViewById(R.id.btn_6);
+        btnlist[6] = (Button)findViewById(R.id.btn_7);
+        btnlist[7] = (Button)findViewById(R.id.btn_8);
+        btnlist[8] = (Button)findViewById(R.id.btn_9);
+        btnlist[9] = (Button)findViewById(R.id.btn_10);
+        btnlist[10] = (Button)findViewById(R.id.btn_11);
+        btnlist[11] = (Button)findViewById(R.id.btn_12);
+        btnlist[12] = (Button)findViewById(R.id.btn_13);
+        btnlist[13] = (Button)findViewById(R.id.btn_14);
+        btnlist[14] = (Button)findViewById(R.id.btn_15);
+        btnlist[15] = (Button)findViewById(R.id.btn_16);
+        btnlist[16] = (Button)findViewById(R.id.btn_17);
 
-        btnAnswerButton[0] = (Button)findViewById(R.id.btn_payday);
-        btnAnswerButton[1] = (Button)findViewById(R.id.btn_using_period);
-        btnAnswerButton[2] = (Button)findViewById(R.id.btn_this_month);
-        btnAnswerButton[3] = (Button)findViewById(R.id.btn_last_month);
-        btnAnswerButton[4] = (Button)findViewById(R.id.btn_amount);
+        txtlist[0] = (TextView)findViewById(R.id.txt_1);
+        txtlist[1] = (TextView)findViewById(R.id.txt_2);
+        txtlist[2] = (TextView)findViewById(R.id.txt_3);
+        txtlist[3] = (TextView)findViewById(R.id.txt_4);
+        txtlist[4] = (TextView)findViewById(R.id.txt_5);
+        txtlist[5] = (TextView)findViewById(R.id.txt_6);
+        txtlist[6] = (TextView)findViewById(R.id.txt_7);
+        txtlist[7] = (TextView)findViewById(R.id.txt_8);
+        txtlist[8] = (TextView)findViewById(R.id.txt_9);
+        txtlist[9] = (TextView)findViewById(R.id.txt_10);
+        txtlist[10] = (TextView)findViewById(R.id.txt_11);
+        txtlist[11] = (TextView)findViewById(R.id.txt_12);
+        txtlist[12] = (TextView)findViewById(R.id.txt_13);
+        txtlist[13] = (TextView)findViewById(R.id.txt_14);
+        txtlist[14] = (TextView)findViewById(R.id.txt_15);
+        txtlist[15] = (TextView)findViewById(R.id.txt_16);
+        txtlist[16] = (TextView)findViewById(R.id.txt_17);
 
-        linearRequestList = (LinearLayout)findViewById(R.id.linear_request_list);
-        linearRequestMoney[0] = (LinearLayout)findViewById(R.id.linear_request_money_head);
-        linearRequestMoney[1] = (LinearLayout)findViewById(R.id.linear_request_money_contents);
-        linearClientList[0] = (LinearLayout)findViewById(R.id.linear_client_list_head);
-        linearClientList[1] = (LinearLayout)findViewById(R.id.linear_client_list_contents);
-        linearDefaultList[0] = (LinearLayout)findViewById(R.id.linear_default_list_head);
-        linearDefaultList[1] = (LinearLayout)findViewById(R.id.linear_default_list_contents);
-        linearUsing[0] = (LinearLayout)findViewById(R.id.linear_using_list);
-        linearUsing[1] = (LinearLayout)findViewById(R.id.linear_using_compare);
-        linearTaxBill[0] = (LinearLayout)findViewById(R.id.linear_tax_bill_head);
-        linearTaxBill[1] = (LinearLayout)findViewById(R.id.linear_tax_bill_contents);
-
-        ((GradientDrawable)linearRequestList.getBackground()).setStroke(2, 0xFF4488DD);
-        ((GradientDrawable)linearRequestMoney[0].getBackground()).setColor(0xFFEE6611);
-        ((GradientDrawable)linearRequestMoney[0].getBackground()).setStroke(2, 0xFFEE6611);
-        ((GradientDrawable)linearRequestMoney[1].getBackground()).setColor(0xFFFFEEDD);
-        ((GradientDrawable)linearRequestMoney[1].getBackground()).setStroke(2, 0xFFEE5511);
-        ((GradientDrawable)linearClientList[0].getBackground()).setColor(0xFF3399EE);
-        ((GradientDrawable)linearClientList[0].getBackground()).setStroke(2, 0xFF3399EE);
-        ((GradientDrawable)linearClientList[1].getBackground()).setColor(0xFFDDEEFF);
-        ((GradientDrawable)linearClientList[1].getBackground()).setStroke(2, 0xFFDDEEFF);
-        ((GradientDrawable)linearDefaultList[0].getBackground()).setColor(0xFF331155);
-        ((GradientDrawable)linearDefaultList[0].getBackground()).setStroke(2, 0xFF331155);
-        ((GradientDrawable)linearDefaultList[1].getBackground()).setStroke(2, 0xFF331155);
-        ((GradientDrawable)linearUsing[0].getBackground()).setColor(0xFF6688FF);
-        ((GradientDrawable)linearUsing[0].getBackground()).setStroke(2, 0xFF6688FF);
-        ((GradientDrawable)linearUsing[1].getBackground()).setColor(0xFF6688FF);
-        ((GradientDrawable)linearUsing[1].getBackground()).setStroke(2, 0xFF6688FF);
-        ((GradientDrawable)linearTaxBill[0].getBackground()).setColor(0xFF6688FF);
-        ((GradientDrawable)linearTaxBill[0].getBackground()).setStroke(2, 0xFF6688FF);
-        ((GradientDrawable)linearTaxBill[1].getBackground()).setStroke(2, 0xFF6688FF);
-
-
-        imgReceipt = (ImageView)findViewById(R.id.img_receipt);
-
-        btnSingleCell[0][0] = (Button)findViewById(R.id.btn_date);
-        btnSingleCell[1][0] = (Button)findViewById(R.id.btn_quantity_1);
-        btnSingleCell[1][1] = (Button)findViewById(R.id.btn_quantity_2);
-        btnSingleCell[1][2] = (Button)findViewById(R.id.btn_quantity_3);
-        btnSingleCell[1][3] = (Button)findViewById(R.id.btn_quantity_4);
-        btnSingleCell[1][4] = (Button)findViewById(R.id.btn_quantity_5);
-        btnSingleCell[2][0] = (Button)findViewById(R.id.btn_price_1);
-        btnSingleCell[2][1] = (Button)findViewById(R.id.btn_price_2);
-        btnSingleCell[2][2] = (Button)findViewById(R.id.btn_price_3);
-        btnSingleCell[2][3] = (Button)findViewById(R.id.btn_price_4);
-        btnSingleCell[2][4] = (Button)findViewById(R.id.btn_price_5);
-        btnSingleCell[3][0] = (Button)findViewById(R.id.btn_tax_amount);
-        btnSingleCell[3][1] = (Button)findViewById(R.id.btn_add_amount);
-        btnSingleCell[3][2] = (Button)findViewById(R.id.btn_total_amount);
-
-        // set button location
-        LinearLayout linearReceipt = (LinearLayout) findViewById(R.id.linear_whole_receipt);
-        FrameLayout.LayoutParams receiptParams = (FrameLayout.LayoutParams)linearReceipt.getLayoutParams();
-
-        Drawable d = getResources().getDrawable(R.drawable.receipt_7_4_4_1);
-        double iImageHeight = d.getIntrinsicHeight();
-        double iImageWidth = d.getIntrinsicWidth();
-
-        Display display = getWindowManager().getDefaultDisplay();
-        double iLayoutHeight = display.getHeight() * 0.8;
-        double iLayoutWidth = display.getWidth() * 0.8;
-
-        double fHeightRatio = iLayoutHeight / iImageHeight;
-        double fWidthRatio = iLayoutWidth / iImageWidth;
-
-        if(fHeightRatio < fWidthRatio){
-            receiptParams.height = (int)(iImageHeight * fHeightRatio);
-            receiptParams.width = (int)(iImageWidth * fHeightRatio);
-        }
-        else{
-            receiptParams.height = (int)(iImageHeight * fWidthRatio);
-            receiptParams.width = (int)(iImageWidth * fWidthRatio);
-        }
-
-        linearReceipt.setLayoutParams(receiptParams);
-        //end of setting
-
-        //button listener for 7_4_1
-        for(int i = 0; i < BUTTON_COUNT; i++){
-            btnAnswerButton[i].setOnClickListener(new View.OnClickListener() {
+        for(int i = 0; i < 17; i++)
+            btnlist[i].setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){
-                    int iBtnIndex = 0;
-                    for(int ii = 0; ii < BUTTON_COUNT; ii++)
-                        if(btnAnswerButton[ii] == v) iBtnIndex = ii;
+                    int i;
+                    Button btnCurrentButton = (Button)v;
 
-                    if(dataSet.iAnswerIndex == iBtnIndex) isRight = true;
+                    for(i = 0 ; i < 17; i++) if(btnCurrentButton == btnlist[i]) break;
+
+                    if(dataSet.iAnswerIndex== i) isRight = true;
                     else isRight = false;
                     checkAnswer();
                 }
             });
-        }
-
-        //button listener for 7_4_2
-        for(int i = 0; i < CELL_ROW; i++)
-            for(int j = 0; j < iRowCount[i]; j++) {
-                btnSingleCell[i][j].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        int iRow = 0, iColumn = 0;
-                        for (int ii = 0; ii < CELL_ROW; ii++)
-                            for(int jj = 0; jj < iRowCount[ii]; jj++)
-                                if (btnSingleCell[ii][jj] == v) { iRow = ii; iColumn = jj; }
-
-                        if (dataSet.iAnswerRow == iRow && dataSet.iAnswerColumn == iColumn) isRight = true;
-                        else isRight = false;
-                        checkAnswer();
-                    }
-                });
-            }
 
 
-        frame7_4_[1].setVisibility(View.INVISIBLE);
         setQuestion(false);
     }
 
     public void setQuestion(boolean isRetry, Object object){
-        dataSet.setData(iStage);
+        int iRandomSeed = iStage - 1;
+
+        dataSet.setData(iRandomSeed);
 
         txtDescription.setText(dataSet.sDescription);
 
-        if(iStage > 3){
-            for(int i = 0; i < CELL_ROW; i++)
-                for(int j = 0; j < iRowCount[i]; j++){
-                    if(dataSet.arrApear[i][j] == 1) btnSingleCell[i][j].setVisibility(View.VISIBLE);
-                    else btnSingleCell[i][j].setVisibility(View.INVISIBLE);
-                }
+        txtlist[1].setText(dataSet.account);
 
-            imgReceipt.setImageResource(dataSet.iReceiptImage);
+        for(int i = 0 ; i < 6 ; i++) {
+            btnlist[8+i].setText(dataSet.money[i]);
+            txtlist[8+i].setText(dataSet.money[i]);
+        }
+
+        for(int i = 0 ; i < 17;i++) {
+            txtlist[i].setVisibility(View.VISIBLE);
+            btnlist[i].setVisibility(View.GONE);
+        }
+
+        for(int i = 0 ; i < 3 ; i++) {
+            txtlist[dataSet.iBtnList[i]].setVisibility(View.GONE);
+            btnlist[dataSet.iBtnList[i]].setVisibility(View.VISIBLE);
         }
 
         StartRecording();
@@ -209,11 +126,6 @@ public class ActStep0704  extends StageActivity {
                     iStage++;
                     iRetryCount = 0;
 
-                    if(iStage == 4){
-                        frame7_4_[0].setVisibility(View.GONE);
-                        frame7_4_[1].setVisibility(View.VISIBLE);
-                    }
-
                     if(iStage <= NUM_OF_STAGE) setQuestion(false);
                     else goNext();
                 }
@@ -225,56 +137,44 @@ public class ActStep0704  extends StageActivity {
     }
 
     public void goNext(Object object){
-        Intent intent = new Intent(this, ActStep0705.class);
+        Intent intent = new Intent(this, ActStep0704.class);
         startActivity(intent);
     }
 
-    public class Step0704DataSet {
-        private final String arrDescription[][] = {{"전기요금 납기일을 나타내는 곳을 찾아 누르세요.",},
-                {"전기요금 청구금액을 나타내는 곳을 찾아 누르세요.",},
-                {"아래 자료에서 당월 전기사용량을 나타내는 곳을 찾아 누르세요.",},
-                {"아래 영수증은 평생마트 영수증입니다. 모듬 버섯 불고기는 얼마인지 찾아 누르세요.", "아래 영수증은 평생마트 영수증입니다. 지불할 금액은 모두 얼마인지 찾아 누르세요.","아래 자료는 평생마트 영수증입니다. 상추는 얼마인지 찾아 누르세요."},
-                {"아래 영수증은 평생마트 영수증입니다. 평생마트에서 물건을 산 날짜는 언제인지 찾아 누르세요.", "아래 영수증은 평생마트 영수증입니다. 평생마트에서 물건을 산 날짜는 언제인지 찾아 누르세요.", "아래 자료는 평생마트 영수증입니다. 오렌지 주스를 몇 개 샀는지 수량을 찾아 누르세요."}
+    public class Step0703DataSet {
+        private final String arrDescription[][]={
+                {"아래 통장에서 계좌번호를 나타내는 곳을 찾아 누르세요.", "아래 통장에서 계좌번호를 찾아 누르세요.", "아래 통장에서 계좌번호를 찾아 누르세요."},
+                {"아래 통장에서 ‘김수현’이  10,000원을 이체한 날짜를 나타내는 곳을 찾아 누르세요.", "아래 통장에서 ‘민형준’이  20,000원을 이체한 날짜를 나타내는 곳을 찾아 누르세요.", "아래 통장에서 ‘남동식’이  10,000원을 이체한 날짜를 나타내는 곳을 찾아 누르세요."},
+                {"아래 통장에서 ‘이제신’이 20,000원을 입금하고 난 후 통장에 남은 금액은 얼마인지 찾아 누르세요.", "아래 통장에서 ‘이제신’이 20,000원을 입금하고 난 후 통장에 남은 금액은 얼마인지 찾아 누르세요.", "아래 통장에서 ‘박형식’이 맡긴 금액은 얼마인지 찾아 누르세요."},
+                {"아래 통장을 보고 ‘남동식’이  10,000원을 입금하고 난 후 통장에 남은 금액은 얼마인지 찾아 누르세요.", "아래 통장을 보고 ‘남동식’이  10,000원을 입금하고 난 후 통장에 남은 금액은 얼마인지 찾아 누르세요.", "아래 통장을 보고 ‘유지은’이  10,000원을 입금하고 난 후 통장에 남은 금액은 얼마인지 찾아 누르세요."},
+                {"아래 통장에서 ‘민형준’이 이체한 날짜를 찾아 누르세요.", "아래 통장에서 ‘남동식’이 이체한 날짜를 찾아 누르세요.", "아래 통장에서 민형준은 얼마를 입금했는지 찾아 누르세요."}
         };
 
-        private final int arrAnswerButtonIndex[] = {0, 4, 2};
-        private final int arrReceiptImage[] = {R.drawable.receipt_7_4_4_1, R.drawable.receipt_7_4_5_1};
+        private final int arrAnswerButtonIndex[][] = {{1,1,1},{3,5,7},{10,10,14},{13,13,12},{5,7,15}};
+        private final int BtnList[][][] ={
+                {{0,1,3},{0,1,3},{0,1,3}},
+                {{3,5,7},{3,5,7},{3,5,7}},
+                {{4,10,12}, {4,10,12},{14,15,16}},
+                {{7,10,13,},{7,10,13},{10,11,12}},
+                {{5,7,10},{3,5,7},{15,16,14}}};
 
-        private final int arrApearList[][][] = {{{0}, {0, 0, 0, 0, 0}, {1, 0, 0, 1, 0}, {0, 0, 0}},
-                {{1}, {0, 0, 0, 0, 0}, {1, 0, 0, 1, 0}, {0, 0, 1}}};
-        private final int arrAnswerButtonRow[] = {2, 0};
-        private final int arrAnswerButtonColumn[] = {0, 0};
+        private final String  accountList[]={"계좌번호 123456-78-9101112", "계좌번호 100037-56-166616", "계좌번호 222-11-4321-123"};
+        private final String  moneyList[][]={{"310,000 ", "320,000 ","340,000 ","360,000 ","370,000 ","380,000 "},{"410,000 ","420,000 ","440,000 ","460,000 ","470,000 ","480,000 "}, {"357,200 ","367,200 ","387,200 ","407,200 ","417,200 ","427,200 "}};
 
         public String sDescription;
-
+        public String account;
         public int iAnswerIndex;
+        public int iBtnList[] = new int[3];
+        public String money[] = new String[6];
 
-        public int arrApear[][] = new int[CELL_ROW][CELL_COLUMN];
-        public int iAnswerRow;
-        public int iAnswerColumn;
-        public int iReceiptImage;
-
-        public void setData(int iStage) {
-            int iSeed = iStage - 1;
+        public void setData(int iSeed)
+        {
             int rand = (int)(Math.random() * 3.0); // 0 ~ 2
-
-
-            if(iSeed < 3)
-            {
-                sDescription = arrDescription[iSeed][0];
-                iAnswerIndex = arrAnswerButtonIndex[iSeed];
-            }
-            else{
-                sDescription = arrDescription[iSeed][rand];
-                iAnswerRow = arrAnswerButtonRow[iSeed - 3];
-                iAnswerColumn = arrAnswerButtonColumn[iSeed - 3];
-                iReceiptImage = arrReceiptImage[iSeed - 3];
-
-                for(int i = 0; i < CELL_ROW; i++)
-                    for(int j = 0; j < iRowCount[i]; j++)
-                      arrApear[i][j] = arrApearList[iSeed - 3][i][j];
-            }
+            sDescription = arrDescription[iSeed][rand];
+            iAnswerIndex = arrAnswerButtonIndex[iSeed][rand];
+            iBtnList = BtnList[iSeed][rand];
+            money = moneyList[rand];
+            account = accountList[rand];
         }
-
     }
 }
