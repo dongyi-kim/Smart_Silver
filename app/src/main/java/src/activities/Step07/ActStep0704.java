b8f7f89705605956985a830469b0ee521ddb5866
adman <jhoboss@gmail.com>
2015-12-06 22:45:19 +0900

    backup..

--- app/src/main/java/src/activities/Step07/ActStep0704.java
+++ app/src/main/java/src/activities/Step07/ActStep0704.java
@@ -14,12 +14,6 @@ import src.dialogs.DlgResultMark;
 
 public class ActStep0704 extends StageActivity {
 
-    private TextView txtDescription;
-    private Button btnlist[] = new Button[18];
-    private TextView txtlist[] = new TextView[18];
-    private String money[] = new String[6];
-    private TextView iaccount;
-
     private int iRetryCount = 0;
     public boolean isRight = false;
 
@@ -67,22 +61,6 @@ public class ActStep0704 extends StageActivity {
     }
 
     public class Step0704DataSet {
-        private final String arrDescription[] = {"전기요금 납기일을 나타내는 곳을 찾아 누르세요.",
-                "전기요금 청구금액을 나타내는 곳을 찾아 누르세요.",
-                "아래 자료에서 당월 전기사용량을 나타내는 곳을 찾아 누르세요.",
-                "아래 영수증은 평생마트 영수증입니다. 모듬 버섯 불고기는 얼마인지 찾아 누르세요.",
-                "아래 영수증은 평생마트 영수증입니다. 평생마트에서 물건을 산 날짜는 언제인지 찾아 누르세요."};
-        private final int arrAnswerButtonIndex[] = {0, 4, 2};
-        private final int arrReceiptImage[] = {R.drawable.receipt_7_4_4_1, R.drawable.receipt_7_4_5_1};
-
-        private final String  accountList[]={"계좌번호 123456-78-9101112", "계좌번호 100037-56-166616", "계좌번호 222-11-4321-123"};
-        private final String  moneyList[][]={{"310,000 ", "320,000 ","340,000 ","360,000 ","370,000 ","380,000 "},{"410,000 ","420,000 ","440,000 ","460,000 ","470,000 ","480,000 "}, {"357,200 ","367,200 ","387,200 ","407,200 ","417,200 ","427,200 "}};
-
-        public String sDescription;
-        public String account;
-        public int iAnswerIndex;
-        public int iBtnList[] = new int[3];
-        public String money[] = new String[6];
 
         public void setData(int iSeed)
         {
